43e94d7f29af213b05afb50b5e7512e20a0f7fae
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-07-07 12:55:22 +0500

    Redux and reducers added

---
+++ client/src/reducers/cartReducer.js
@@ -0,0 +1,52 @@
+import {
+  ADD_TO_CART,
+  REMOVE_CART_ITEM,
+  SAVE_SHIPPING_INFO,
+} from "../constants/cartConstant";
+
+// Cart Reducer
+
+export const cartReducer = (
+  state = { cartItems: [], shippingInfo: {} },
+  action
+) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      // eslint-disable-next-line no-case-declarations
+      const item = action.payload;
+      
+      // eslint-disable-next-line no-case-declarations
+      const isItemExist = state.cartItems.find(
+        (i) => i.product === item.product
+      );
+
+      if (isItemExist) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((i) =>
+            i.product === isItemExist.product ? item : 1
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+
+    case REMOVE_CART_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((i) => i.product !== action.payload),
+      };
+
+    case SAVE_SHIPPING_INFO:
+      return {
+        ...state,
+        shippingInfo: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
